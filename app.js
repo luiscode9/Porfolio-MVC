9ee83f9f292e6f77d586368d3b0219e89ca551e8
luisbossa <luisbossa@gmail.com>
2024-10-14 17:49:07 -0600

    little changes

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const nodemailer = require("nodemailer");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 app.use("/resources", express.static("public"));
 app.use("/resources", express.static(__dirname + "/public"));
