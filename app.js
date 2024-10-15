902b96f86987b9d13504dc6af8a23a669c446a77
luisbossa <luisbossa@gmail.com>
2024-10-15 02:09:40 -0600

    attempt to deply

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const serverless = require("serverless-http");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/resources", express.static("public"));
 app.use("/resources", express.static(__dirname + "/public"));
@@ -13,5 +16,8 @@ app.set("view engine", "ejs");
 app.use(require("./src/routes/index.router"));
 app.use(require("./src/routes/form.router"));
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+app.use("/.netlify/app");
+module.exports.handler = serverless(app);
+
+/*const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));*/
