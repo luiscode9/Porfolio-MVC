d66ac9e588b1aebdb61d6bd12a72fb9f3348cf37
luisbossa <luisbossa@gmail.com>
2024-10-15 03:39:13 -0600

    fgg

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const indexController = require("../controllers/index.controller");
 
-router.get("/.netlify/functions/", indexController.index);
+router.get("/", indexController.index);
 
 module.exports = router;
