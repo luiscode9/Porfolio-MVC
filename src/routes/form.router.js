bc732b5ec977d7f9ddd4d42b4d4e92143af8c3ab
luisbossa <luisbossa@gmail.com>
2024-10-15 03:30:28 -0600

    tt

--- src/routes/form.router.js
+++ src/routes/form.router.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const formController = require("../controllers/form.controller");
 
-router.post("/", formController.send);
+router.post("/.netlify/functions/", formController.send);
 
 module.exports = router;
--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const indexController = require("../controllers/index.controller");
 
-router.get("/", indexController.index);
+router.get("/.netlify/functions/", indexController.index);
 
 module.exports = router;
