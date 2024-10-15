b6ac0219befa43a5ed15106ff2775a503f132e06
luisbossa <luisbossa@gmail.com>
2024-10-15 02:29:53 -0600

    deploy

--- functions/app.js
+++ functions/app.js
@@ -2,21 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const serverless = require("serverless-http");
+
 const app = express();
 
+// Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use("/resources", express.static("public"));
-app.use("/resources", express.static(__dirname + "/public"));
 
 app.set("view engine", "ejs");
 
-app.use(require("./src/routes/index.router"));
-app.use(require("./src/routes/form.router"));
+// Routes
+app.use(require("../src/routes/index.router"));
+app.use(require("../src/routes/form.router"));
 
-app.use("/.netlify/app");
+// Export handler for serverless
 module.exports.handler = serverless(app);
 
 /*const PORT = process.env.PORT || 3001;
